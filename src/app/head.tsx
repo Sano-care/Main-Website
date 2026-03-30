87ab255668e0654c80894934840e2d79cdbc6f8f
Amartya Singh <amartya22062@iiitd.ac.in>
2026-03-30 20:30:50 +0530

    Add Head component with Facebook domain verification meta

---
+++ src/app/head.tsx
@@ -0,0 +1,11 @@
+// src/app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="facebook-domain-verification"
+        content="kfszus13qiekuol0cdt8784x8bdgb6"
+      />
+    </>
+  )
+}
